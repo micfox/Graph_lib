50096c96bccd9f3be6ae1da852d9661e8eda3105
Jiateng <jiatengwu@qq.com>
2017-01-18 13:44:58 +0800

    add Box.h

--- Box.h
+++ Box.h
@@ -1,38 +1,33 @@
-
 namespace Graph_lib {
-struct Box : Shape {
-	Box(Point p, int width, int height, int radius){
-		if(radius*2 > min(width,height))
-			error("corner radius exceed line length");
-		straight_line.add({p.x + radius,p.y},{p.x+width-radius,p.y}); //top
-		straight_line.add({p.x + radius,p.y+height},{p.x+width-radius,p.y+height}); //bot
-		straight_line.add({p.x, p.y+radius},{p.x,p.y+height-radius}); //left
-		straight_line.add({p.x + width, p.y+radius},{p.x+width,p.y+height-radius}); //right
-		round_corner.push_back(new Arc {{p.x + radius, p.y + radius}, radius, radius, 90, 180});
-		round_corner.push_back(new Arc {{p.x + width - radius, p.y + radius}, radius, radius, 0, 90});
-		round_corner.push_back(new Arc {{p.x + radius, p.y + height - radius}, radius, radius, 180, 270});
-		round_corner.push_back(new Arc {{p.x + width - radius, p.y + height - radius}, radius, radius, 270, 360});
-//		arc_test = new Arc {{p.x + width/2, p.y + height/2}, radius, radius, 0, 360};
-
-		
+struct Box : Lines {
+	Box(Point sp, int ww, int hh, int rr)
+		:start_point{sp}, width{ww}, height{hh}, 
+		diameter{2 * rr}, round_radius{rr} {
+		add(Point{sp.x + rr, sp.y}, Point{sp.x + ww - rr, sp.y});
+		add(Point{sp.x + rr, sp.y + hh}, Point{sp.x + ww - rr, sp.y + hh});
+		add(Point{sp.x, sp.y +rr}, Point{sp.x, sp.y + hh - rr});
+		add(Point{sp.x + ww, sp.y + rr}, Point{sp.x + ww, sp.y + hh - rr});
 	}
 	void draw_lines() const {
-			straight_line.draw_lines();	
-		arc_test.draw_lines();
-		for(int a = 0; a < round_corner.size(); ++a)
-			round_corner[a].draw_lines();
+		Lines::draw_lines();
+		fl_arc(start_point.x, start_point.y, 
+				2 * round_radius, 2 * round_radius, 90, 180);
+		fl_arc(start_point.x + width - diameter, start_point.y, 
+				2 * round_radius, 2 * round_radius, 0, 90);
+		fl_arc(start_point.x, start_point.y + height - diameter, 
+				2 * round_radius, 2 * round_radius, 180, 270);
+		fl_arc(start_point.x + width - diameter, 
+				start_point.y + height - diameter,
+				2 * round_radius, 2 * round_radius, 270, 360); 
 	}
-/*
-	void set_color(Color col) {
-		straight_line.set_color(col);
-		for(int i; i < round_corner.size(); ++i)
-			round_corner[i].set_color(col);
-	}
-*/
-private:
-	Lines straight_line;
-	Arc arc_test {{100, 100}, 80, 80, 0, 360};
-	Vector_ref<Arc> round_corner;
-};
 
+
+private:
+	Point start_point;
+	int round_radius;
+	int diameter; // diameter of round corner
+	int width;
+	int height;
+};
 }
+
