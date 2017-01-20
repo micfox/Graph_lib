a44907c26a338fe9ba468450cf736b08363df381
Jiateng <jiatengwu@qq.com>
2017-01-20 11:16:03 +0800

    finish exer 1308

--- 1308.cpp
+++ 1308.cpp
@@ -4,31 +4,44 @@
 #include <cmath>
 
 #define PI 3.14159265
+
+Point tl {100,100};
+Simple_window win {tl,600,400,"test"};
+
 Point p {0,0};
-int r = 50;
+static int r = 50;
+static int y_shift = static_cast<int>( round( 2 * r * sin (60 * PI /180))); // short radius
+static int x_shift = static_cast<int>( round( 3 * r / 2 ));
+bool col_odd = true;
 
 void get_next_point()
 //get position for next hexagon
 {
-	int d = static_cast<int> (rount (2 * r * sin (60 * PI /180)));
-	p.y += d;
-	if ( (p.y - r) > win.y_max() ) {
-		p.x += d;
-		p.y = //if odd col, y add a shift, if even col, y = 0
+	p.y += y_shift;
+	if ( p.y > win.y_max() + y_shift ) {
+		p.x += x_shift;
+		if (col_odd) { 
+			//if odd col, y add a shift, if even col, y = 0
+			p.y = y_shift / -2;
+		} else {
+			p.y = 0;
+		}
+		col_odd = !col_odd;
 	}  
 }
 
 int main()
 {
 	using namespace Graph_lib;
-	Point tl {100,100};
-	Simple_window win {tl,600,400,"test"};
 
-	Point p {0,0};
-	int r = 50;
+	Vector_ref<Regular_hexagon> foo;
+	//how many Regs we will display?
+	while( p.x < win.x_max() + r ) {
+		foo.push_back( new Regular_hexagon {p, r} );	
+		get_next_point();
+	}
 
-	Regular_hexagon foo {{200,200}, 50};
-	foo.set_fill_color (Color::white);
-	win.attach(foo);
+	for (int i=0; i < foo.size(); ++i)
+		win.attach(foo[i]);
 	win.wait_for_button();
 }
