16b220936707be9b09ec6df45512cffaf1b809fc
Jiateng <jiatengwu@qq.com>
2017-01-23 16:27:58 +0800

    update file

---
+++ Smiley.h
@@ -0,0 +1,11 @@
+
+namespace Graph_lib {
+struct Smiley : Circle {
+	
+	void draw_lines() {
+		//calculate 3 position
+		//then draw 3 circles
+		Circle c {{center().x, center().y + radius() / 
+	}
+};
+}
