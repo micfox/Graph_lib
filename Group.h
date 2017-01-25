f6ac7fb6a3c6fd046626851b61ffb3f8bcf3f275
Jiateng <jiatengwu@qq.com>
2017-01-25 17:49:10 +0800

    Group.h

---
+++ Group.h
@@ -0,0 +1,35 @@
+namespace Graph_lib {
+struct Group : Shape {
+public:
+//	Group() {}
+/*
+//dunno how to use initlist
+	Group(initializer_list<Shape&> lst) {
+		for (auto s : lst)
+			shapes.push_back(s);
+	}
+*/
+	void draw() const {
+		for (int i=0; i < shapes.size(); ++i)
+			shapes[i].draw();	
+	}
+	void add(Shape& s) {shapes.push_back(s);}
+	void move(int x, int y) {
+		for (int i=0; i < shapes.size(); ++i)
+			shapes[i].move(x, y);
+	}	
+	void set_color(Color c) {
+		for (int i=0; i < shapes.size(); ++i)
+			shapes[i].set_color(c);
+	}
+	int count() {return shapes.size();}
+	Shape& get(int i) {
+		if (i < 0 || i > shapes.size())
+			error("out of bound");
+		return shapes[i];
+	}
+private:
+	void set_point();
+	Vector_ref<Shape> shapes;
+};
+}
