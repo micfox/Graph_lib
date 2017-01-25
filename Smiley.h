6f7ab7abab3f37231c1c08e17b0841d4ab7f49af
Jiateng <jiatengwu@qq.com>
2017-01-25 08:54:32 +0800

    no care

--- Smiley.h
+++ Smiley.h
@@ -1,11 +1,19 @@
 
 namespace Graph_lib {
-struct Smiley : Circle {
-	
-	void draw_lines() {
+struct Smiley : Closed_polyline {
+	Smiley(initializer_list<Point> lst) :Closed_polyline{lst}, c1{{100,100}, 20} {}	
+//	using Closed_polyline::Closed_polyline;
+	void draw_lines() const {
+		Closed_polyline::draw_lines();
 		//calculate 3 position
 		//then draw 3 circles
-		Circle c {{center().x, center().y + radius() / 
+		c1.draw_lines();	
 	}
+private:
+//	Circle c1 {{center().x - 20, center().y - 20}, 20};
+//	Circle c1 {{100,100}, 20};
+	Circle c1;
+	int i = 0;
+	int b;
 };
 }
